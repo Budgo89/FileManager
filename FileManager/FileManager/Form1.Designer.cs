311a2078ee0536b4e8501e5e299bc014d9ea11d1
Maks Sapunov <Budgo@mail.ru>
2021-12-29 11:57:28 +0300

    первые наброски

--- FileManager/FileManager/Form1.Designer.cs
+++ FileManager/FileManager/Form1.Designer.cs
@@ -28,20 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.mainWindow = new System.Windows.Forms.ListBox();
+            this.fileAddress = new System.Windows.Forms.TextBox();
+            this.goOver = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // mainWindow
+            // 
+            this.mainWindow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainWindow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mainWindow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mainWindow.FormattingEnabled = true;
+            this.mainWindow.ItemHeight = 20;
+            this.mainWindow.Location = new System.Drawing.Point(23, 84);
+            this.mainWindow.Name = "mainWindow";
+            this.mainWindow.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.mainWindow.Size = new System.Drawing.Size(758, 404);
+            this.mainWindow.TabIndex = 0;
+            // 
+            // fileAddress
+            // 
+            this.fileAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fileAddress.Location = new System.Drawing.Point(23, 34);
+            this.fileAddress.Name = "fileAddress";
+            this.fileAddress.Size = new System.Drawing.Size(758, 22);
+            this.fileAddress.TabIndex = 1;
+            // 
+            // goOver
+            // 
+            this.goOver.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.goOver.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.goOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.goOver.Location = new System.Drawing.Point(803, 32);
+            this.goOver.Name = "goOver";
+            this.goOver.Size = new System.Drawing.Size(120, 23);
+            this.goOver.TabIndex = 2;
+            this.goOver.Text = "Перейти";
+            this.goOver.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(935, 536);
+            this.Controls.Add(this.goOver);
+            this.Controls.Add(this.fileAddress);
+            this.Controls.Add(this.mainWindow);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox mainWindow;
+        private System.Windows.Forms.TextBox fileAddress;
+        private System.Windows.Forms.Button goOver;
     }
 }
 
